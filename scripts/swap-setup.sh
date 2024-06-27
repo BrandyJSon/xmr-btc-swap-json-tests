38fa01c60146ef35d0625d49ff198bce66791c13
BrandyJSon <twiddle@example.com>
2024-06-27 11:42:26 -0400

    Add submodule

--- scripts/swap-setup.sh
+++ scripts/swap-setup.sh
@@ -2,8 +2,10 @@
 
 docker-compose up -d
 
+mkdir -p ../electrumx-testnet4/datadir
+
 cd ~/electrumx
-DB_DIRECTORY=../electrumx/datadir DAEMON_URL=http://demo:demo@127.0.0.1:5000 COIN=Bitcoin NET=regtest SERVICES=tcp://:50001 ../electrumx/electrumx_server
+DB_DIRECTORY=../electrumx-testnet4/datadir DAEMON_URL=http://demo:demo@127.0.0.1:5000 COIN=Bitcoin NET=regtest SERVICES=tcp://:50001 ../electrumx-testnet4/electrumx_server
 
 cd ~/
 #docker exec -it bitcoind /bin/bash
